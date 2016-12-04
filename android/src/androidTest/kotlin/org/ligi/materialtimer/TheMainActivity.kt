421c91fce28328c2f69ce2e87db862eb97ca8451
ligi <ligi@ligi.de>
2016-12-05 00:35:33 +0100

    Use trulesk

--- android/src/androidTest/kotlin/org/ligi/materialtimer/TheMainActivity.kt
+++ android/src/androidTest/kotlin/org/ligi/materialtimer/TheMainActivity.kt
@@ -3,35 +3,27 @@ package org.ligi.materialtimer
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.rule.ActivityTestRule
-import android.view.WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-import android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.ligi.materialteatimer.MainActivity
 import org.ligi.materialteatimer.R
+import org.ligi.trulesk.TruleskActivityRule
 
 
 class TheMainActivity {
 
     @get:Rule
-    val activityTestRule = ActivityTestRule(MainActivity::class.java)
-
-    @Before
-    fun setUp() {
-        activityTestRule.activity.runOnUiThread { activityTestRule.activity.window.addFlags(FLAG_TURN_SCREEN_ON or FLAG_DISMISS_KEYGUARD) }
-    }
+    val rule = TruleskActivityRule(MainActivity::class.java)
 
     @Test
     fun thatActivityShouldLaunch() {
-        ScreenShotTaker.takeScreenShot(activityTestRule.activity,"main")
+        rule.screenShot("main")
     }
 
     @Test
     fun thatInfoOpens() {
         onView(withId(R.id.menuInfo)).perform(click())
 
-        ScreenShotTaker.takeScreenShot(activityTestRule.activity,"info")
+        rule.screenShot("info")
     }
 }
