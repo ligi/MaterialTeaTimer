fe900802a88a667fffb1fbe0f0aff76b6653a27f
ligi <ligi@ligi.de>
2016-12-05 01:51:37 +0100

    Shorten Code

--- android/src/main/java/org/ligi/materialteatimer/TeaAdapter.kt
+++ android/src/main/java/org/ligi/materialteatimer/TeaAdapter.kt
@@ -5,18 +5,14 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 
 class TeaAdapter : RecyclerView.Adapter<TeaViewHolder>() {
-    val teaList = TeaProvider.teas
 
-    override fun onBindViewHolder(holder: TeaViewHolder, position: Int) {
-        holder.bind(teaList[position])
-    }
+    override fun onBindViewHolder(holder: TeaViewHolder, position: Int)
+            = holder.bind(TeaProvider.teas[position])
 
-    override fun getItemCount(): Int {
-        return teaList.size
-    }
+    override fun getItemCount() = TeaProvider.teas.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeaViewHolder {
-        return TeaViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.tea_card, parent, false))
-    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeaViewHolder
+            = TeaViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.tea_card, parent, false))
 
 }
